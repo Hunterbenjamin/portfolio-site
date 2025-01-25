7b672fa604afa9356992b93746de6e9bf67513e8
Hunterbenjamin <Hunterlbenjamin@gmail.com>
2025-01-26 01:07:28 +0800

    Initial commit: Added project files

---
+++ purgecss 12.58.32 AM.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    content: ["./*.html", "./js/**/*.js"], // Paths to your HTML and JS files
+    css: ["./css/styles.css"], // Path to your CSS file(s)
+    output: "./css", // Where the cleaned CSS file should go
+};
